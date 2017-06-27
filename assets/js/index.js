cdf846453ca772151bd7ecdfd0eed28db4c6e074
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-06-27 13:12:58 -0500

    css okey, solo falta subir algunos divs

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+/*const getAlumnasList= (url) => {
+  $.get(url,(response) => {
+    console.log(response);
+  })
+};
+getAlumnasList('http://laboratoria.cuadra.co:9339/api/v1/students/?format=json');*/
+
+'use strict';
+
+const getJSON = (url, cb) => {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    if(xhr.status !== 200){
+      return cb(new Error('Error loading JSON from ' + url + '(' + xhr.status + ')'));
+    }
+
+    cb(null, xhr.response);
+  });
+
+  xhr.open('GET', url);
+  xhr.responseType = 'json';
+  xhr.send();
+};
+
+
+$( _ => {
+
+  getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/?format=json', (err, json) => {
+    if (err) {
+      return alert(err.message);
+      state.coders = json;
+    }
+  });
+});
